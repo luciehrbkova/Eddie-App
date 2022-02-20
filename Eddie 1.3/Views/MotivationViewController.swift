add827cd15f391cd4f893b72d4585ddc4feb10f1
luciehrbkova <50968340+luciehrbkova@users.noreply.github.com>
2022-02-20 22:37:41 +0000

    database reding/writing working

--- Eddie 1.3/Views/MotivationViewController.swift
+++ Eddie 1.3/Views/MotivationViewController.swift
@@ -6,25 +6,85 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class MotivationViewController: UIViewController {
+class MotivationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ref: DatabaseReference?
+    var databaseHandle: DatabaseHandle?
+    var postData = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        title = "Motivation"
-        // Do any additional setup after loading the view.
+        
+        
+        // Set Delegates-----------------
+        tableView.delegate = self
+        tableView.dataSource = self
+        textField.delegate = self
+        // Set Database------------------
+        ref = Database.database().reference()
+        
+        //Retreive the posts and listen for changes (.childAdded)
+        databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            // Code to executed when a child is added under "Posts"
+            // Tak the value from the snapsot and added it into postData array
+            // Try to convert the value of the data to a string
+            let post = snapshot.value as? String
+            // conditional bonding testing if there is data
+            if let actualPost = post {
+                //Append data to our post data Array
+                self.postData.append(actualPost)
+                // Reload the table view
+                self.tableView.reloadData()
+            }
+        })
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addPost(_ sender: Any) {
+        //TODO: Post the data to firebase
+        //create automatically child by id
+        if textField.text != "" {
+            ref?.child("Posts").childByAutoId().setValue(textField.text)
+            print(textField.text!)
+            textField.text?.removeAll()
+        }
+        //dismiss keyboard
+        textField.endEditing(true)
     }
-    */
-
+    
+// TableView Delegates methods-------------------------------------------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        cell.textLabel?.text = postData.reversed()[indexPath.row]
+        return cell
+    }
+    
+// TextField Delegates methods-------------------------------------------------
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        print(textField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
 }
